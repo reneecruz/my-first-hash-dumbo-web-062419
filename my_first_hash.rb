d7c563f5eac0467cd6be5d02de58b95de71eb1f3
Renée Cruz <reneebasscruz@gmail.com>
2019-06-17 06:22:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,10 +1,13 @@
 def my_hash
   your_hash = {"my_name" => "Renée", "my_age" => '37', "my_sun_sign" => "Aquaius"}
+  
 end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
+  the_manifest = {}
+  
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
